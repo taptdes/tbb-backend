b647f39d2debe9cf7e0739c0ca4855d065afb188
Sara Tapusoa <sara.tapusoa@gmail.com>
2025-11-14 12:37:35 -0700

    fix: tbb-61 remove all instances of sanityClient

--- dist/src/lib/cms/urlFor.js
+++
@@ -1,4 +0,0 @@
-import imageUrlBuilder from "@sanity/image-url";
-import { getSanityClient } from "./sanityClient";
-const builder = imageUrlBuilder(getSanityClient());
-export function urlFor(source) { return builder.image(source); }
